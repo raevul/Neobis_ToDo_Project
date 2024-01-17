8ac7efa1b45ef607454226ac55b06d201a42e50e
Ular <raevular01@gmail.com>
2024-01-17 18:31:18 +0600

    added new field 'created_at', this ordering task by time and fixed bugs

--- todo/main/models.py
+++ todo/main/models.py
@@ -5,6 +5,10 @@ class Task(models.Model):
     title = models.CharField(max_length=50)
     is_complete = models.BooleanField("Complete", default=False)
     description = models.TextField(null=True, blank=True)
+    created_at = models.TimeField("Created at", auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
 
     def __str__(self):
         return self.title
